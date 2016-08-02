6c681c3faca9ea225b3d1567e831a5b4c6d010f2
Hendrik Brummermann <nhnb@users.sourceforge.net>
2016-08-02 22:06:34 +0200

    fixed build script to include missing font files

--- resources/build.sh
+++ resources/build.sh
@@ -6,6 +6,7 @@ rm -rf dist
 mkdir -p dist/postsai
 cp -ax *.html *.js *.md *.py *.txt resources dist/postsai
 cp --parents node_modules/bootstrap/dist/css/bootstrap.min.css node_modules/bootstrap/dist/js/bootstrap.min.js                    dist/postsai
+cp --parents node_modules/bootstrap/dist/fonts/*                                                          dist/postsai
 cp --parents node_modules/bootstrap-table/dist/bootstrap-table.min.css node_modules/bootstrap-table/dist/bootstrap-table.min.js   dist/postsai
 cp --parents node_modules/bootstrap-table/dist/extensions/export/bootstrap-table-export.min.js            dist/postsai
 cp --parents node_modules/bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile.min.js            dist/postsai
