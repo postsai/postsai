18fbee3d4447b6cf7de9ac6a5ceb85b15ca2fa5c
Hendrik Brummermann <nhnb@users.sourceforge.net>
2016-08-06 01:18:55 +0200

    do not intend example of configuration file

--- install.py
+++ install.py
@@ -18,31 +18,31 @@ class PostsaiInstaller:
         help_config_file = """
     Please create a file called config.py with this content an run install.py again: 
 
-    import os
+import os
 
-    db = {
-        "host" : "localhost",
-        "user" : "postsaiuser",
-        "password" : "postsaipassword",
-        "database" : "postsaidb"
-    }
+db = {
+    "host" : "localhost",
+    "user" : "postsaiuser",
+    "password" : "postsaipassword",
+    "database" : "postsaidb"
+}
 
-    ui = {
-        "avatar" : "https://gravatar.com",
-        "trim_email" : True
-    }
+ui = {
+    "avatar" : "https://gravatar.com",
+    "trim_email" : True
+}
 
-    def get_read_permission_pattern():
-        \"""return a regular expression of repository names that may be read\"""
+def get_read_permission_pattern():
+    \"""return a regular expression of repository names that may be read\"""
 
-        # return os.environ.get("AUTHENTICATE_POSTSAI_READ_PATTERN", "^$")
-        return ".*"
+    # return os.environ.get("AUTHENTICATE_POSTSAI_READ_PATTERN", "^$")
+    return ".*"
 
-    def get_write_permission_pattern():
-        \"""return a regular expression of repository names that may be written to\"""
+def get_write_permission_pattern():
+    \"""return a regular expression of repository names that may be written to\"""
 
-        # return os.environ.get("AUTHENTICATE_POSTSAI_WRITE_PATTERN", "^$")
-        return ".*"
+    # return os.environ.get("AUTHENTICATE_POSTSAI_WRITE_PATTERN", "^$")
+    return ".*"
     """
         print(help_config_file)
         sys.exit(1)
