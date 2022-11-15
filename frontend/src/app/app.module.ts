a3851da0a7896b87b62f366cad45e2e3379e615f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2022-11-15 21:45:34 +0100

    created angular project with material design

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		LayoutModule,
+		MatButtonModule,
+		MatListModule,
+		MatIconModule,
+		MatTableModule,
+		MatToolbarModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
