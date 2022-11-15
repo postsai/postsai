e583ff3ebebc4bea69bd942d5483024287c03b9a
Hendrik Brummermann <nhnb@users.sourceforge.net>
2022-11-16 00:06:33 +0100

    fixed linebreak and vertical scrolling

---
+++ frontend/src/app/result/data.ts
@@ -0,0 +1,43 @@
+export var mockdata = {
+	"additional_scripts": [],
+	"config": {
+		"service_worker": false,
+		"trim_email": false
+	},
+	"data": [
+		[
+			"something/somethingelse/something.somethingelse.repository",
+			"2022-11-15 15:17:15",
+			"someone_with_a_long_email_address@example.com",
+			[
+				"something/something/someotherthing/languages/international/english.txt"
+			],
+			[
+				"dbcab2425803a31f5f15af28f6a31ddbfc02608e"
+			],
+			"Version_2022.11.123123.312312",
+			"0/0",
+			"added additional translations for a number of elements (#290392)",
+			"something/somethingelse/something.somethingelse.repository",
+			"dbcab2425803a31f5f15af28f6a31ddbfc02608e",
+			""
+		],
+		[
+			"a/repository",
+			"2022-11-15 15:14:39",
+			"somebody@example.com",
+			[
+				"src/test/Test"
+			],
+			[
+				"f5aab4565b592ef32306c5449fa247a09ed2abc5"
+			],
+			"",
+			"0/0",
+			"#290216 Abnahme Testfall 19870",
+			"a/repository",
+			"f5aab4565b592ef32306c5449fa247a09ed2abc5",
+			""
+		]
+	]
+};
--- frontend/src/app/result/result.component.ts
+++ frontend/src/app/result/result.component.ts
@@ -1,52 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { mockdata } from './data';
 
 @Component({
 	selector: 'app-result',
 	templateUrl: './result.component.html'
 })
 export class ResultComponent {
-	public data = {
-		"additional_scripts": [],
-		"config": {
-			"service_worker": false,
-			"trim_email": false
-		},
-		"data": [
-			[
-				"a/repository",
-				"2022-11-15 15:17:15",
-				"someone@example.com",
-				[
-					"foo/bar/i18n_en.txt"
-				],
-				[
-					"dbcab2425803a31f5f15af28f6a31ddbfc02608e"
-				],
-				"RELEASE_2022_12",
-				"0/0",
-				"removed old i18n-key #290392\n",
-				"a/repository",
-				"dbcab2425803a31f5f15af28f6a31ddbfc02608e",
-				""
-			],
-			[
-				"a/repository",
-				"2022-11-15 15:14:39",
-				"somebody@example.com",
-				[
-					"src/test/Test"
-				],
-				[
-					"f5aab4565b592ef32306c5449fa247a09ed2abc5"
-				],
-				"",
-				"0/0",
-				"#290216 Abnahme Testfall 19870",
-				"a/repository",
-				"f5aab4565b592ef32306c5449fa247a09ed2abc5",
-				""
-			]
-		]
-	};
+	public data = mockdata;
+
 	public columnsToDisplay = ["repository", "when", "who", "file", "commit", "branch", "description"];
+	
+	breaks(value?: string) {
+		if (!value) {
+			return value;
+		}
+		return value.toString().replace(/([A-Z/_.@])/g, "\u200b$1");
+	}
+
+	formatTimestamp(value?: string) {
+		if (!value) {
+			return "-";
+		}
+		return value.substring(0, 16);
+	}
+
+	@HostListener('copy', ['$event'])
+	onCopyHandler(event: ClipboardEvent) {
+		let originalText = window.getSelection();
+		if (originalText) {
+			let text = originalText.toString().replace(/[\u200b-\u200d\ufeff]/g, '');
+			event.clipboardData!.setData('text/plain', text);
+			event.preventDefault();
+		}
+	}
+
+
 }
